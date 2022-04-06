7106baba9075145d230346fcf6ede8edac677104
AndreiGatoCB <98832349+AndreiGatoCB@users.noreply.github.com>
2022-04-05 21:14:08 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_EMAIL = "myemail@gmail.com"
+PASSWORD = "******"
+MY_LAT = -34.593802
+MY_LONG = -58.397133
+
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = int(data["iss_position"]["latitude"])
+    iss_longitude = int(data["iss_position"]["longitude"])
+
+    #Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+# Then send me an email to tell me to look up.
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=PASSWORD)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs="bbbbb@unal.edu.co",
+                                msg="subject:Mira arriba\n\nLa estación espacial internacional está acá.")
+# BONUS: run the code every 60 seconds.
+
+
+
